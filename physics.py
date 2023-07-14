e2c644dd9fc2657d917f01ae59b6bd48eeebc8d0
Jianmin Chen <jianmin@jianminchen.com>
2023-07-14 03:36:03 -0400

    Update physics + past tests

--- physics.py
+++ physics.py
@@ -1,6 +1,9 @@
+from numpy import sin, cos, pi
+
 # Constants
 g = 9.81
 density_water = 1000
+surface_pressure = 101325
 
 
 def calculate_buoyancy(V: float, density_fluid: float):
@@ -16,7 +19,8 @@ def calculate_buoyancy(V: float, density_fluid: float):
     if V < 0 or density_fluid <= 0:
         raise ValueError("Volume and/or density of fluid should be > 0")
 
-    return density_fluid * V * g
+    buoyancy_force = density_fluid * V * g
+    return buoyancy_force
 
 
 def will_it_float(V: float, mass: float):
@@ -34,7 +38,10 @@ def will_it_float(V: float, mass: float):
         raise ValueError("Volume and/or mass should be > 0")
 
     # mass / V = density; if density < density_water, it will float, otherwise will sink
-    if mass / V < density_water:
+    density = mass / V
+    if density == density_water:
+        return None
+    elif density < density_water:
         return True
     return False
 
@@ -52,7 +59,123 @@ def calculate_pressure(depth: float):
     if depth <= 0:
         raise ValueError("Depth should be > 0 meters")
 
-    return density_water * g * depth
+    pressure = density_water * g * depth + surface_pressure
+    return pressure
+
+
+def calculate_acceleration(F: float, m: float) -> float:
+    """
+    Calculates the acceleration of an object given the force applied to it and its mass
+
+        Parameters:
+            F (float): The force applied to the object in Newtons
+            m (float): The mass of the object in kg.
+
+        Returns:
+            a (float): The acceleration of the object.
+    """
+
+    if m <= 0:
+        raise ValueError("Mass should be > 0 kilograms")
+    if F < 0:
+        raise ValueError("Force should be >= 0 Newtons")
+
+    a = F / m
+    return a
+
+
+def calculate_angular_acceleration(tau: float, I: float) -> float:
+    """
+    Calculates the angular acceleration of an object given the torque applied to it and its moment of inertia.
+
+        Parameters:
+            tau (float): The torque applied to the object in Newton-meters
+            I (float): the moment of inertia of the object in kg * m^2.
+
+        Returns:
+            angular_acceleration (float)
+    """
+
+    if I <= 0:
+        raise ValueError("Moment of inertia should be > 0")
+    if tau < 0:
+        raise ValueError("Torque should be > 0")
+
+    # angular_acceleration = r / I
+    angular_acceleration = tau / I
+    return angular_acceleration
+
+
+def calculate_torque(F_magnitude: float, F_direction: float, r: float) -> float:
+    """
+    Calculates the torque applied to an object given the force applied to it and the stance from the axis of rotation to the point where the force is applied.
+
+        Parameters:
+            F_magnitude (float): The magnitude of the force applied to the object in Newtons
+            F_direction (float): The direction of the force applied to the object in degrees
+            r (float): The distance from the axis of rotation to the point where the force is applied in meters
+
+        Returns:
+            torque (float)
+    """
+
+    # r = r * F
+    force = F_magnitude * sin(F_direction * (pi / 180))
+    torque = r * force
+    return torque
+
+
+def calculate_moment_of_inertia(m: float, r: float) -> float:
+    """
+    Calculates the moment of inertia of an object given its mass and the distance from the axis of rotation to the center of mass of the object.
+
+        Parameters:
+            m (float): The mass of the object in kilograms.
+            r (float): The distance from the axis of rotation to the center of mass of the object in meters.
+
+        Returns:
+            moment_of_inertia (float)
+    """
+
+    moment_of_inertia = m * (r**2)
+    return moment_of_inertia
+
+
+def calculate_auv_acceleration(
+    F_magnitude: float,
+    F_angle: float,
+    mass: float = 100,
+    volume: float = 0.1,
+    thruster_distance: float = 0.5,
+):
+    """
+    Calculates the acceleration of the AUV in the 2D plane.
+
+        Parameters:
+            F_magnitude (float): The magnitude of force applied by the thruster in Newtons
+            F_angle (float): The angle of the force applied by the thruster in radians. The angle is measured from the x-axis. Positive angles are measured from the x-axis. Positive angles are measured in the counter-clockwise direction.
+            mass (float): The mass of the AUV in kilograms.
+            volume (float): The volume of the AUV in cubic meters.
+            thruster_distance (float): The distance from the center of mass of the AUV to the thruster in meters.
+
+        Returns:
+            acceleration (float): Acceleration of the AUV
+    """
+
+    # a = F / m
+    acceleration = F_magnitude / mass
+    return acceleration
+
+
+def calculate_auv_angular_acceleration(F_magnitude: float, F_angle: float) -> float:
+    """
+    Calculates the angular acceleration of the AUV.
+
+        Parameters:
+            F_magnitude (float)
+    """
+
+    pass
 
 
 if __name__ == "__main__":
--- test_physics.py
+++ test_physics.py
@@ -4,14 +4,15 @@ import physics
 
 class TestPhysics(unittest.TestCase):
     def test_constants(self):
-        self.assertEquals(g, 9.81)
-        self.assertEquals(density_water, 1000)
+        self.assertEqual(physics.g, 9.81)
+        self.assertEqual(physics.density_water, 1000)
+        self.assertEqual(physics.surface_pressure, 101325)
 
     def test_calculate_buoyancy(self):
         self.assertRaises(ValueError, physics.calculate_buoyancy(-5, 6))
         self.assertRaises(ValueError, physics.calculate_buoyancy(6, -5))
-        self.assertEquals(physics.calculate_buoyancy(1, density_water), 9810)
-        self.assertEquals(physics.calculate_buoyancy(60, density_water), 588600)
+        self.assertEqual(physics.calculate_buoyancy(1, physics.density_water), 9810)
+        self.assertEqual(physics.calculate_buoyancy(60, physics.density_water), 588600)
 
     def test_will_it_float(self):
         self.assertRaises(
@@ -21,15 +22,42 @@ class TestPhysics(unittest.TestCase):
         self.assertRaises(ValueError, physics.will_it_float(5, -5))
         self.assertTrue(physics.will_it_float(50, 5))
         self.assertFalse(physics.will_it_float(9, 10000))
-        self.assertFalse(
+        self.assertIsNone(
             physics.will_it_float(10, 10000)
         )  # = density water, so neither floats nor sinks
 
     def test_calculate_pressure(self):
         self.assertRaises(ValueError, physics.calculate_pressure(-10))
         self.assertRaises(ValueError, physics.calculate_pressure(0))
-        self.assertEquals(physics.calculate_pressure(1), 9810)
-        self.assertEquals(physics.calculate_pressure(3657), 35875170)
+        self.assertEqual(physics.calculate_pressure(1), 9810 + physics.surface_pressure)
+        self.assertEqual(
+            physics.calculate_pressure(3657), 35875170 + physics.surface_pressure
+        )
+
+    def test_calculate_acceleration(self):
+        self.assertRaises(ValueError, physics.calculate_acceleration(0, 0))  # m != 0
+        self.assertRaises(ValueError, physics.calculate_acceleration(0, -1))  # m != < 0
+        self.assertRaises(ValueError, physics.calculate_acceleration(-1, 1))  # F != < 0
+        self.assertEqual(physics.calculate_acceleration(0, 5), 0)
+        self.assertEqual(physics.calculate_acceleration(10, 2), 5)
+
+    def test_calculate_angular_acceleration(self):
+        self.assertRaises(ValueError, physics.calculate_angular_acceleration(1, 0))
+        self.assertRaises(ValueError, physics.calculate_angular_acceleration(1, -1))
+        self.assertRaises(ValueError, physics.calculate_angular_acceleration(-1, 1))
+        self.assertEqual()
+
+    def test_calculate_torque(self):
+        pass
+
+    def test_calculate_moment_of_inertia(self):
+        pass
+
+    def test_calculate_auv_acceleration(self):
+        pass
+
+    def test_calculate_auv_angular_acceleration(self):
+        pass
 
 
 if __name__ == "__main__":
